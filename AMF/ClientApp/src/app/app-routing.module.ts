9ba2e42bb84efaf89a954b335de2d03ef19e9240
b6016268@my.shu.ac.uk <mununuribonjesi@Mununuris-MBP.connect>
2019-04-07 21:57:11 +0100

    created pages for haze billz website
    s

--- AMF/ClientApp/src/app/app-routing.module.ts
+++ AMF/ClientApp/src/app/app-routing.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { EventsComponent } from './components/events/events.component';
+import { VideosComponent } from './components/videos/videos.component';
+import { ContactComponent } from './components/contact/contact.component';
 
 const routes: Routes = [
-    {path:'home',component: HomeComponent }
+{  path:'home',component: HomeComponent},
+    {path:'events',component: EventsComponent},
+        {path:'videos',component: VideosComponent},
+        {path:'contact',component: ContactComponent},
     ];
 
 @NgModule({
--- AMF/ClientApp/src/app/app.module.ts
+++ AMF/ClientApp/src/app/app.module.ts
@@ -6,11 +6,19 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { RouterModule } from '../../node_modules/@angular/router';
 import {PaginatorModule} from 'primeng/paginator';
+import { EventsComponent } from './components/events/events.component';
+import { VideosComponent } from './components/videos/videos.component';
+import { LsComponent } from './components/ls/ls.component';
+import { ContactComponent } from './components/contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
       HomeComponent,
+      EventsComponent,
+      VideosComponent,
+      LsComponent,
+      ContactComponent,
          
   ],
   imports: [
