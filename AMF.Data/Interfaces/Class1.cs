754425a4b0576a82e9c2a4daad4cb53b59f1ca67
mbonjesi <mbonjesi@IT-2>
2019-06-06 11:05:01 +0100

    folder pattern

---
+++ AMF.Data/Interfaces/Class1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AMF.Data.Interfaces
+{
+    class Class1
+    {
+    }
+}
--- AMF/Startup.cs
+++ AMF/Startup.cs
@@ -1,18 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using AMF.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.SpaServices.AngularCli;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 
 namespace AMF
 {
