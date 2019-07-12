17d5f60b349f31ee38b09f022c0cce6c7706632a
mbonjesi <mbonjesi@IT-2>
2019-07-12 15:57:43 +0100

    created the new shop page for the site

--- AMF/ClientApp/src/app/app-routing.module.ts
+++ AMF/ClientApp/src/app/app-routing.module.ts
@@ -4,13 +4,15 @@ import { HomeComponent } from './components/home/home.component';
 import { EventsComponent } from './components/events/events.component';
 import { VideosComponent } from './components/videos/videos.component';
 import { ContactComponent } from './components/contact/contact.component';
+import { ShopComponent } from './components/shop/shop.component';
 
 const routes: Routes = [
     { path:'', redirectTo:'/home',pathMatch:'full'},
     { path:'home',component: HomeComponent},
     { path:'events',component: EventsComponent},
-    { path:'videos',component: VideosComponent},
-    { path:'contact',component: ContactComponent},
+  { path: 'videos', component: VideosComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'shop', component: ShopComponent }
     ];
 
 @NgModule({
--- AMF/ClientApp/src/app/app.module.ts
+++ AMF/ClientApp/src/app/app.module.ts
@@ -5,29 +5,33 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { RouterModule } from '../../node_modules/@angular/router';
-import {PaginatorModule} from 'primeng/paginator';
+import { PaginatorModule } from 'primeng/paginator';
 import { EventsComponent } from './components/events/events.component';
 import { VideosComponent } from './components/videos/videos.component';
 import { ContactComponent } from './components/contact/contact.component';
 import { MailingListService } from './services/MailingListService';
-import { HttpClientModule } from '@angular/common/http'; 
+import { HttpClientModule } from '@angular/common/http';
+import { ShopComponent } from './components/shop/shop.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-      HomeComponent,
-      EventsComponent,
-      VideosComponent,
-      ContactComponent,
-         
+    HomeComponent,
+    EventsComponent,
+    VideosComponent,
+    ContactComponent,
+    ShopComponent
+
+
+
   ],
   imports: [
-      BrowserModule,
-      AppRoutingModule,
-      RouterModule,
-      PaginatorModule,
-      HttpClientModule
+    BrowserModule,
+    AppRoutingModule,
+    RouterModule,
+    PaginatorModule,
+    HttpClientModule
   ],
   providers: [MailingListService],
   bootstrap: [AppComponent]
