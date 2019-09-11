689a71f6a5be891490cae5f7c0e4a5eadce9a6be
mbonjesi <mbonjesi@IT-2>
2019-09-11 17:21:17 +0100

    changed ccompability version to 2.2

--- AMF/Startup.cs
+++ AMF/Startup.cs
@@ -29,7 +29,7 @@ namespace AMF
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
             services.AddEntityFrameworkNpgsql().AddDbContext<DatabaseContext>(opt => opt.UseNpgsql(Configuration.GetConnectionString("DefaultConnection")));
             services.AddSpaStaticFiles(configuration => { configuration.RootPath = "ClientApp/dist"; });
             services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
