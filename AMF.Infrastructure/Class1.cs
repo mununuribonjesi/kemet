fb6d175be3767a3e5f03441146550fa7750bd557
mbonjesi <mbonjesi@IT-2>
2019-09-12 13:52:33 +0100

    just added class librabries to the project

---
+++ AMF.Infrastructure/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace AMF.Infrastructure
+{
+    public class Class1
+    {
+    }
+}
--- AMF/Startup.cs
+++ AMF/Startup.cs
@@ -1,9 +1,4 @@
 ﻿using System;
-using AMF.Data.Interfaces.Repositories;
-using AMF.Data.Repositories;
-using AMF.Entities.Models;
-using AMF.Infrastructure.Interfaces;
-using AMF.Infrastructure.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
