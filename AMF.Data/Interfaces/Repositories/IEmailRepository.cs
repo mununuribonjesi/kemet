5b76bda75c250e2a1a8debe88c119c4bda99273f
mbonjesi <mbonjesi@IT-2>
2019-09-12 10:36:18 +0100

    removed files in the project setup

--- AMF.Data/Interfaces/Repositories/IEmailRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using AMF.Entities.DTOs;
-
-namespace AMF.Data.Interfaces.Repositories
-{
-    public interface IEmailRepository: IRepository<SubscribedEmails>
-    {
-        new void Add(SubscribedEmails Mail);
-    }
-}
